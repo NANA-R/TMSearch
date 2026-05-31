aa0616fdab501511ed64fd0e287f86cf6dc14177
HelLgato <195056137+NANA-R@users.noreply.github.com>
2026-05-31 15:26:53 +0900

    fix app

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import Select
 from flask import Flask, request
 import time
 import unicodedata
+import os
 
 options = Options()
 options.add_argument('--headless')
@@ -152,4 +153,5 @@ def searching():
       pass
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
